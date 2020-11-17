ca5b53c2cacf22f844a4c18fe8aca97d8a0b1f20
kanako-110 <kanako.110.ito@gmail.com>
2020-11-17 20:32:40 +0900

    画面遷移ask

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import Header from "./Header";
+import Home from "./Home";
+import AskList from "./ask/AskList";
+import Footer from "./Footer";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import ChatList from "./ChatList";
+import Chat from "./Chat";
+import Profile from "./Profile";
+import SignOut from "./SignOut";
+import About from "./About";
+import EditAsk from "./ask/EditAsk";
+import PostAsk from "./ask/PostAsk";
+import AskDetails from "./ask/AskDetails";
+
+const App = () => {
+	return (
+		<div>
+			<Router>
+				<Header />
+				<Switch>
+					{" "}
+					{/* 1つのコンポーネントのみ返すように設定 */}
+					<Route path="/" exact component={Home} />
+					<Route path="/asklist" exact component={AskList} />
+					<Route path="/login" exact component={LogIn} />
+					<Route path="/signup" exact component={SignUp} />
+					<Route path="/chatlist" exact component={ChatList} />
+					<Route path="/chat" exact component={Chat} />
+					<Route path="/profile" exact component={Profile} />
+					<Route path="/signout" exact component={SignOut} />
+					<Route path="/about" exact component={About} />
+					<Route path="/editask" exact component={EditAsk} />
+					<Route path="/postask" exact component={PostAsk} />
+					<Route path="/askdetails/:id" exact component={AskDetails} />
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<p>----ここからHome----</p>
+			<button>詳しく見る</button>
+			<button>今すぐ始める</button>
+		</div>
+	);
+};
+
+export default Home;
