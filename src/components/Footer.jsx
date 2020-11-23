c6d45654ef29df51e9d1c1ecdf1634ed137a58b5
kanako-110 <kanako.110.ito@gmail.com>
2020-11-23 09:53:25 +0900

    footerの導入

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
-		<div>
-			<hr />
-			<Link to="/about">Stayfulとは?</Link>
-			<p>ご利用ガイド</p>
-			<Link to="/asklist">ASK</Link>
-			<Link to="/asklist">HELP</Link>
-			<p>ミッションとビジョン</p>
-			<p>ご利用規約</p>
-			<p>プライバシーポリシー</p>
-			<p>お問い合わせ</p>
-			<p>アンケート</p>
+		<div style={{display: "flex", backgroundColor: "#2f4f4f", height:"25vh"}}>
+			<div>
+				<Link to="/about">Stayfulとは?</Link>
+				<p>ご利用ガイド</p>
+				<Link to="/asklist">ASK</Link>
+				<Link to="/asklist">HELP</Link>
+				<p>お知らせ</p>
+				<p>ミッションとビジョン</p>
+			</div>
+			<div>
+				<p>ご利用規約</p>
+				<p>プライバシーポリシー</p>
+				<p>お問い合わせ</p>
+				<p>アンケート</p>
+				<p>Twitter</p>
+			</div>
 		</div>
 	);
 };
