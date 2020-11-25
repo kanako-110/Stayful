02b9b172174923a7335f21787b6271e171ec73da
kanako-110 <kanako.110.ito@gmail.com>
2020-11-25 14:45:01 +0900

    header修正。LogIn後はLogoutのみ出す #24

--- src/components/pages/ask/AskList.jsx
+++ src/components/pages/ask/AskList.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
+import styled from "styled-components";
+
+const TOP_DIV = styled.div`
+	padding-top: 10%;
+	height: 100vh;
+`;
 
 const AskList = (props) => {
 	// 投稿を押したらその投稿に飛ぶ。importとexportにwithRouterも追加
@@ -9,14 +15,17 @@ const AskList = (props) => {
 	};
 
 	return (
-		<div>
+		<div className="ENTIRE_DIV">
+		<TOP_DIV>
 			<h2>相談する</h2>
 			<Link to="editask">自分の投稿を編集</Link>
+			<br />
 			<Link to="postask">投稿する</Link>
 			<button onClick={() => onDivClick()}>
 				アメリカで銀行をつくりたいです
 			</button>
 			　{/* onClickでその投稿ページ(askDetails)に行く */}
+		</TOP_DIV>
 		</div>
 	);
 };
---
+++ src/components/pages/profile/ShowProfile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShowProfile() {
+	return (
+		<div style={{ paddingTop: "10%", height: "90vh" }}>
+			あなたのプロフィール
+		</div>
+	);
+}
