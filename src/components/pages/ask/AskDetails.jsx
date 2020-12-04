879469a07ca2f26f61d1ceabe1f38bdec56b3d6f
kanako-110 <kanako.110.ito@gmail.com>
2020-12-04 11:11:18 +0900

    askDetailsにて、その投稿の内容を表示。 #42

--- src/components/pages/ask/AskDetails.jsx
+++ src/components/pages/ask/AskDetails.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import firebase from "../../../firebase/firebase";
 import styled from "styled-components";
 import Button from "../../atoms/Button";
 
-const CONTAINER = styled.div`
-	position: absolute;
-	left: 38%;
-	top: 30%;
-	width: 56%;
+const POSITION_CONTAINER = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding-top: 10%;
 `;
 
 const ASK_BOX = styled.div`
+	width: 55%;
 	background-color: #ffffff;
 	height: 30vh;
 	padding: 5% 3%;
@@ -19,39 +19,58 @@ const ASK_BOX = styled.div`
 `;
 
 const TITLE = styled.h6`
-	font-size: 1.3rem;
-	padding-bottom: 2%;
-	&:hover {
-		color: #696969;
-	}
+	font-size: 1.4rem;
+	padding-bottom: 7%;
 `;
 
+const P = styled.p `
+font-size: 1.2rem;
+`
+
 const PROFILE_BOX = styled.div`
-	position: absolute;
-	top: 30%;
 	width: 27%;
 	height: 28vh;
 	background-color: #ffffff;
 `;
 
 const AskDetails = () => {
-	const history = useHistory();
-	const onButtonClick = () => {
-		history.push("/chat");
-	};
+	const { id } = useParams();
+	const [askDetail, set_askDetail] = useState("");
+	console.log(id);
+
+	useEffect(() => {
+		firebase
+			.firestore()
+			.collection("ask")
+			.get()
+			.then((data) => {
+				const asks = data.docs.map((doc) => {
+					return doc.data();
+				});
+				set_askDetail(asks.find((ask) => ask.askId == id));
+			});
+	}, []);
+
+	console.log(askDetail);
+
 	return (
 		<div className="COLOR_POSITION" style={{ height: "80vh" }}>
 			<div className="ENTIRE_DIV">
-				<PROFILE_BOX></PROFILE_BOX>
-				<CONTAINER>
+				<POSITION_CONTAINER>
+					<PROFILE_BOX></PROFILE_BOX>
 					<ASK_BOX>
-						<h3>相談内容</h3>
-						<p>アメリカでの銀行の作り方を教えて下さい</p>
+						<TITLE> {askDetail.title} </TITLE>
+						<P> {askDetail.detail} </P>
 					</ASK_BOX>
-					<div style={{ width: "100%" }}>
-						<Button text="メッセージでHELPする" top="110%" left="70%" link="/chat" />
-					</div>
-				</CONTAINER>
+				</POSITION_CONTAINER>
+				<div style={{ width: "100%" }}>
+					<Button
+						text="メッセージでHELPする"
+						top="85%"
+						left="76.5%"
+						link="/chat"
+					/>
+				</div>
 			</div>
 		</div>
 	);
