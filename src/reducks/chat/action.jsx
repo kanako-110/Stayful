a9eac5f87a4eb61085d5dacbb7c6e18af6ec7d43
kanako-110 <kanako.110.ito@gmail.com>
2020-12-08 08:48:33 +0900

    message　消去ボタン作成 #52

--- src/reducks/chat/action.jsx
+++ src/reducks/chat/action.jsx
@@ -3,8 +3,8 @@ export const fetch_messages = (messageAllData) => {
 	return {
 		type: FETCH_MESSAGES,
 		payload: messageAllData,
-	}
-}
+	};
+};
 
 export const CREATE_MESSAGE = "CREATE_MESSAGE";
 export const create_message = (message) => {
@@ -14,3 +14,10 @@ export const create_message = (message) => {
 	};
 };
 
+export const DELETE_MESSAGE = "DELETE_MESSAGE";
+export const delete_message = (message) => {
+	return {
+		type: DELETE_MESSAGE,
+		payload: message,
+	};
+};
