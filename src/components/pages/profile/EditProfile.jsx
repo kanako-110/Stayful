20d4e2cac1a35cc14a450d78f10a8d41e9fc15e0
kanako-110 <kanako.110.ito@gmail.com>
2020-12-10 20:44:20 +0900

    プロフィールページ　UIデザイン作成 #60

--- src/components/pages/profile/EditProfile.jsx
+++ src/components/pages/profile/EditProfile.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar"; //ひとまずのimport
+import Button from "../../atoms/Button";
+import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import styled from "styled-components";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import TranslateIcon from "@material-ui/icons/Translate";
+import PublicIcon from "@material-ui/icons/Public";
 
 const useStyles = makeStyles((theme) => ({
 	large: {
-		width: theme.spacing(7),
-		height: theme.spacing(7),
+		width: theme.spacing(15),
+		height: theme.spacing(15),
 	},
 	root: {
 		display: "flex",
@@ -18,13 +23,27 @@ const useStyles = makeStyles((theme) => ({
 		marginRight: theme.spacing(1),
 		width: "25ch",
 	},
+	margin: {
+		margin: theme.spacing(1),
+	},
 }));
+
+const PROFILE_EDITBOX = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const H3 = styled.h3`
+	padding-bottom: 3%;
+	color: #f5b47a;
+	font-size: 1.3rem;
+`;
 
 const EDIT_BOX = styled.div`
 	background-color: #ffffff;
-	width: 80%;
-	height: 65vh;
+	height: 59vh;
 	margin: auto;
+	padding: 1% 2%;
 `;
 
 export default function EditProfile() {
@@ -32,14 +51,81 @@ export default function EditProfile() {
 	return (
 		<div className="COLOR_POSITION" style={{ height: "80vh" }}>
 			<div className="ENTIRE_DIV">
-				<h3>プロフィールを編集する</h3>
-				<Avatar src="/broken-image.jpg" className={classes.large} />
-				<EDIT_BOX>
-					<p>name: kanako ito</p>
-					<p>使える言語：　英語・スペイン語</p>
-					<p>精通している国：カナダ・英語・スペイン語</p>
-					<p></p>
-				</EDIT_BOX>
+				<PROFILE_EDITBOX>
+					<div>
+						<Avatar src="/broken-image.jpg" className={classes.large} />
+					</div>
+					<div style={{ width: "80%" }}>
+						<H3>プロフィールを編集する</H3>
+						<EDIT_BOX>
+							<div>
+								<TextField
+									className={classes.margin}
+									id="input-with-icon-textfield"
+									label="ユーザーネーム"
+									style={{ width: "45%" }}
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<br />
+								<TextField
+									className={classes.margin}
+									id="input-with-icon-textfield"
+									label="話せる言語"
+									style={{ width: "45%" }}
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<TranslateIcon />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<br />
+								<TextField
+									className={classes.margin}
+									id="input-with-icon-textfield"
+									label="精通している国"
+									helperText="(住んだことのある国や、旅行したことのある国など)"
+									style={{ width: "45%" }}
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<PublicIcon />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<TextField
+									name="detail"
+									id="outlined-full-width"
+									label="ひとこと"
+									helperText="(あなたの性格や、得意分野などを書いておくとAskerから高評価を得やすいです。)"
+									style={{ margin: 8, width: "60%" }}
+									multiline
+									rows={7}
+									margin="normal"
+									InputLabelProps={{
+										shrink: true,
+									}}
+									variant="outlined"
+									// inputRef={register({ required: true })}
+								/>
+							</div>
+						</EDIT_BOX>
+					</div>
+				</PROFILE_EDITBOX>
+				<Button
+					text="変更する"
+					left="86%"
+					marginTop="1%"
+					link="/yourprofile/:name"
+				/>
 			</div>
 		</div>
 	);
