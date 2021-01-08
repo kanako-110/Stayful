8f27c28d4652910ce38a09af10c6a1b3b348c023
kanako-110 <kanako.110.ito@gmail.com>
2021-01-08 14:47:51 +0900

    mailList修正　feature#75

--- src/components/pages/chat/ChatList.jsx
+++ src/components/pages/chat/ChatList.jsx
@@ -17,88 +17,30 @@ const H3 = styled.div`
 const TITLE = styled.p`
 	padding-bottom: 5%;
 	font-size: 1.1rem;
+	font-weight: bold;
 `;
 
-const ChatList = () => {
+const TITLE_DETAIL = styled.p`
+	padding-bottom: 5%;
+`;
+
+export default function ChatList() {
 	const messages = useSelector((state) => state.messages);
-	const asks = useSelector((state) => state.asks);
 	const user = useContext(AuthContext);
-	const [messagesData, set_messagesData] = useState([]);
 	const thisUserMessages =
 		user && messages.filter((message) => message.userId === user.uid);
 
-	useEffect(() => {
-		if (user) {
-			firebase
-				.firestore()
-				.collection("messages")
-				.where("userId", "==", user.uid)
-				.orderBy("createdAt", "asc")
-				.get()
-				.then((data) => {
-					const response = data.docs.map((doc) => {
-						return {
-							pageId: doc.data().pageId,
-							askTitle: doc.data().askTitle,
-						};
-					});
-					console.log(response);
-					set_messagesData(response);
-				});
-		}
-	}, [user]);
-
-	console.log(messagesData);
-
-	const mapMessages = () => {
-		thisUserMessages &&
-			thisUserMessages.map((thisUserMessage) => {
-				return [
-					{
-						pageId: thisUserMessage.pageId,
-						askTitle: thisUserMessage.askTitle,
-					},
-				];
-			});
-	};
-
-	// ------------------------いままで
 	const askTitle =
 		thisUserMessages &&
 		thisUserMessages.map((thisUserMessage) => {
 			return thisUserMessage.askTitle;
 		});
+
 	const withOutDuplicate_askTitles =
-		mapMessages() &&
-		mapMessages().filter(function (x, i, self) {
+		askTitle &&
+		askTitle.filter(function (x, i, self) {
 			return self.indexOf(x) === i;
 		});
-	console.log(withOutDuplicate_askTitles);
-
-	const renderTitle = () => {
-		if (withOutDuplicate_askTitles) {
-			for (let i = 0; i < withOutDuplicate_askTitles.length; i++) {
-				return <p> {withOutDuplicate_askTitles[i]} </p>;
-			}
-		}
-	};
-
-	renderTitle();
-
-	// set_title( thisUserMessages && thisUserMessages.map((thisUserMessage) => {
-	// 					return thisUserMessage.askTitle}))
-
-	// useEffect(() => {
-	// 	if (thisUserMessages && thisUserMessages.length > 0) {
-	// 		set_title(
-	// 			thisUserMessages.map((thisUserMessage) => {
-	// 				return thisUserMessage.askTitle;
-	// 			})
-	// 		);
-	// 	}
-	// }, [user]);
-
-	// console.log(title);
 
 	return (
 		<div className="ENTIRE_DIV" style={{ paddingTop: "10%", height: "100vh" }}>
@@ -106,9 +48,9 @@ const ChatList = () => {
 			<div style={{ display: "flex", justifyContent: "space-between" }}>
 				<div style={{ width: "40%" }}>
 					<TITLE>タイトル</TITLE>
-					{thisUserMessages &&
-						thisUserMessages.map((thisUserMessage) => {
-							return <p> {thisUserMessage.askTitle} </p>;
+					{withOutDuplicate_askTitles &&
+						withOutDuplicate_askTitles.map((askTitle) => {
+							return <TITLE_DETAIL> {askTitle} </TITLE_DETAIL>;
 						})}
 				</div>
 				<div style={{ paddingRight: "3%", textAlign: "center" }}>
@@ -122,6 +64,4 @@ const ChatList = () => {
 			</div>
 		</div>
 	);
-};
-
-export default ChatList;
+}
